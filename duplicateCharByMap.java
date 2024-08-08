c0ef4902093314c8f4d9a0515a5a062c800abd42
prabhakalamegam <prabha7716@gmail.com>
2024-08-09 00:52:11 +0800

    Duplicate Char Example

---
+++ duplicateCharByMap.java
@@ -0,0 +1,34 @@
+package streams;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class duplicateCharByMap {
+
+    public static void main(String[] args) {
+
+        String input = "prabhavathy Kalamegam";
+
+        findDuplicateCharacters(input);
+    }
+
+    private static void findDuplicateCharacters(String input) {
+
+        Map<Character, Integer> charCountMap = new HashMap<>();
+
+
+        // Count the occurrences of each character
+        for (char c : input.toCharArray()) {
+            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+        }
+
+        // Print characters that appear more than once
+        System.out.println("Duplicate characters in the string:");
+
+        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + ": " + entry.getValue() + " times");
+            }
+        }
+    }
+}
---
+++ duplicateCharStream.java
@@ -0,0 +1,23 @@
+package streams;
+
+// find the duplicate characters in list of string
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class duplicateCharStream {
+
+    public static void main(String[] args) {
+
+        String input="prabhavathy Kalamegam";
+        List<String> duplicateCharacter = findDuplicateCharacter(input);
+        System.out.println("Duplicate character::::"+duplicateCharacter);
+    }
+    private static List<String> findDuplicateCharacter(String input){
+        return Arrays.stream(input.split("")).collect(Collectors.groupingBy(ch->ch , Collectors.counting()))
+                .entrySet().stream().filter(cha->cha.getValue()>1).map(Map.Entry::getKey).collect(Collectors.toList());
+    }
+
+}
